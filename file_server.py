fd037213f2b1be01cd2cc35260466c8a70003714
LareinaLi <32418242+LareinaLi@users.noreply.github.com>
2017-12-06 06:12:47 +0800

    Update file_server.py

--- file_server.py
+++ file_server.py
@@ -1,7 +1,12 @@
 import socket
-port_number = 8080
+import threadpool
+import file_server_model
 
+port_number = 8080
 ip_address = socket.gethostbyname(socket.gethostname())
+
+server_thread_pool = threadpool.ThreadPool(500)
+file_system_manager = file_server_model.FileSystemManager('FileSystemDirectory')
 
 def create_server_socket():
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -12,6 +17,38 @@ def create_server_socket():
 
     while True:
         connection, client_address = sock.accept()
+        server_thread_pool.add_task(start_client_interaction, connection)
 
+def start_client_interaction(connection):
+    try:
+        client_id = file_system_manager.add_client(connection)
+        while True:
+            data = connection.recv(1024).decode()
+            split_data = data.split('////')
+            
+            if data == 'KILL_SERVICE':
+                response = 'Killing Service'
+                connection.sendall(response.encode())
+                connection.close()
+                os._exit(0)
+                
+            elif split_data[0] == 'exit':
+                if len(split_data) == 1:
+                    file_system_manager.disconnect_client(connection, client_id)
+                else:
+                    error_response(connection, 1)
+            else:
+                error_response(connection, 1)
+    except:
+        error_response(connection, 0)
+        connection.close()
+        
+def error_response(connection, error_code):
+    if error_code == 0:
+        response = 'Server error\n'
+    if error_code == 1:
+        response = 'Unrecognised command\n'
+    connection.sendall(response.encode())
+        
 if __name__ == '__main__':
     create_server_socket()
